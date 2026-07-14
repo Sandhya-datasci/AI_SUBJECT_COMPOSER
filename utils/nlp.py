f82a0638b752fc3b0fb06a867fd53c99a6025f42
Sandhya-datasci <sandhyachoubey0205@gmail.com>
2026-07-14 20:37:20 +0530

    Fix NLTK deployment

--- utils/nlp.py
+++ utils/nlp.py
@@ -1,14 +1,38 @@
 import re
+import nltk
 from rake_nltk import Rake
 from textblob import TextBlob
 
+# Download NLTK resources only if they are missing
+try:
+    nltk.data.find("corpora/stopwords")
+except LookupError:
+    nltk.download("stopwords")
+
+try:
+    nltk.data.find("tokenizers/punkt")
+except LookupError:
+    nltk.download("punkt")
+
+# For newer NLTK versions (used by some deployments)
+try:
+    nltk.data.find("tokenizers/punkt_tab")
+except LookupError:
+    try:
+        nltk.download("punkt_tab")
+    except:
+        pass
+
+
 def clean_text(text):
     return re.sub(r'\s+', ' ', text).strip()
+
 
 def extract_keywords(text):
     r = Rake()
     r.extract_keywords_from_text(text)
     return r.get_ranked_phrases()[:10]
+
 
 def get_sentiment(text):
     return TextBlob(text).sentiment.polarity
